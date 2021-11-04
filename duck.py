f2d084ddea387b207c501b586537f3f81efc6f27
lukowskimarcin <lukowskimarcin@gmail.com>
2021-11-04 15:07:54 +0100

    Update duck.py
    h

--- duck.py
+++ duck.py
@@ -1,16 +1,18 @@
 # docker run -d -p 8000:8000  rasa/duckling
 import requests
 
-def clearData(input: str, stopWord = "") -> str:
+def clearData(input: str, dims=["number"], stopWord="") -> str:
     data = {
-    'locale': 'pl_PL',
-    'text': input
+        'locale': 'pl_PL',
+        'text': input
     }
     response = requests.post('http://localhost:8000/parse', data=data)
     for entity in response.json():
-        input = input.replace(entity['body'], stopWord)
+        if(entity['dim'] in dims):
+            input = input.replace(entity['body'], stopWord)
 
-    return input.strip()
+    return " ".join(input.split())
 
-input =  "test dwa testx"
-print(clearData(input))    
+
+input = "x trzy   test  dwa testx   maj xx"
+print(clearData(input))
